5a1f3afda0ebb916251e9e8127df9c9ff74136b8
Aleesssino <alesg3a1b4o1@gmail.com>
2024-12-04 01:51:47 +0100

    feat: Add cutom background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <h1 className="text-red-300 text-2xl">hello</h1>
-    </>
+    <div className="relative min-h-screen bg-radial-custom">
+      <div className="p-4 text-center text-slate-50">
+        <h1 className="text-2xl">hello</h1>
+      </div>
+    </div>
   );
 }
 
