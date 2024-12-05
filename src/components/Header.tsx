cd9bc019b89bccf0b4b8d218b4e94bb2af763406
Aleesssino <alesg3a1b4o1@gmail.com>
2024-12-05 22:45:27 +0100

    feat: Add mobile menu navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,21 @@
 import logo from "../assets/meta_movie_logo.jpg";
 import { NavLink } from "react-router-dom";
 import MovieSearch from "./MovieSearch";
-import { Dice6, Menu } from "lucide-react";
+import { Dice6, Menu, X } from "lucide-react";
 import { useState } from "react";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if ((e.target as HTMLDivElement).id === "menu-overlay") {
+      setIsMenuOpen(false);
+    }
+  };
+
   return (
     <>
-      <header className="flex justify-between items-center text-slate-50 py-3 px-8 md:px-60  drop-shadow-md">
+      <header className="flex justify-between items-center text-slate-50 py-3 px-8 md:px-60 z-50 drop-shadow-md">
         <NavLink to="/">
           <img
             src={logo}
@@ -66,6 +72,64 @@ const Header = () => {
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         />
       </header>
+
+      {isMenuOpen && (
+        <div
+          id="menu-overlay"
+          onClick={handleOverlayClick}
+          className="bg-radial-custom backdrop-blur-sm fixed inset-0  bg-black z-30 flex  justify-center"
+        >
+          <button
+            className="absolute top-4 right-4 text-slate-50"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            <X />
+          </button>
+          <nav className="flex flex-col items-center space-y-6 py-4 px-8  md:hidden pt-16">
+            <ul className="text-4xl font-semibold text-slate-50 w-full">
+              <li>
+                <NavLink
+                  to="/favorites"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-blue-500"
+                      : "text-slate-50 hover:text-sky-300"
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Favorites
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/popular"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-blue-500"
+                      : "text-slate-50 hover:text-sky-300"
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Popular
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/popular"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-blue-500"
+                      : "text-slate-50 hover:text-sky-300"
+                  }
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  <Dice6 className="w-8 h-8" />
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </>
   );
 };
