ad9f45efe283eff58cf852439e520aaea402f1af
Aleesssino <alesg3a1b4o1@gmail.com>
2024-12-07 21:07:27 +0100

    chore: Clean folder

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,4 +1,4 @@
-import Movie from "../../types/TMovie";
+import Movie from "../types/TMovie";
 
 const apiKey = import.meta.env.VITE_API_KEY as string;
 const base_url = "https://api.themoviedb.org/3";
@@ -16,4 +16,12 @@ export const fetchPopularMovies = async (): Promise<Movie[]> => {
   }
 
   return [];
+};
+
+export const fetchMovieDetails = async (id: string): Promise<Movie> => {
+  const response = await fetch(`${base_url}/movie/${id}?api_key=${apiKey}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch movie details");
+  }
+  return response.json();
 };
