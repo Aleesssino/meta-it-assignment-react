583f1014a09c8eac23bd579431b756ab0fd004cb
Aleesssino <alesg3a1b4o1@gmail.com>
2024-12-07 21:13:33 +0100

    chore: Update Home with MovieGrid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import MovieCard from "../components/MovieCard";
 import { useSearch } from "../contexts/useSearch";
 import Movie from "../types/TMovie";
 import { fetchPopularMovies } from "../service/api";
+import MovieGrid from "../components/MovieGrid";
 
 export default function Home() {
+  const { setMovies } = useSearch();
+
   const {
     data: movies = [],
     isLoading,
@@ -14,35 +16,27 @@ export default function Home() {
     queryFn: fetchPopularMovies,
   });
 
-  // Debug
-  const ll = fetchPopularMovies();
-  console.log({ movies, isLoading, error, ll });
+  if (movies.length > 0) {
+    setMovies(movies);
+  }
 
-  // const [searchQuery, setSearchQuery] = useState("");
-  // const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   e.preventDefault();
-  //   alert(searchQuery);
-  // };
-
-  const { searchQuery } = useSearch();
-
-  const filteredMovies = movies.filter((movie) =>
-    movie.title.toLowerCase().includes(searchQuery.toLowerCase()),
-  );
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error loading movies.</p>;
+  if (isLoading)
+    return (
+      <p className="flex justify-center text-slate-50 text-center pt-14 text-xl">
+        Loading...
+      </p>
+    );
+  if (error)
+    return (
+      <p className="text-slate-50 justify-center text-center">
+        Error loading movies.
+      </p>
+    );
 
   return (
     <div>
       <h1 className="text-2xl text-center py-3 text-slate-50">Meta Movies</h1>
-      <div className="flex justify-center items-center py-5">
-        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 place-items-center gap-4">
-          {filteredMovies.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
-        </div>
-      </div>
+      <MovieGrid />
     </div>
   );
 }
